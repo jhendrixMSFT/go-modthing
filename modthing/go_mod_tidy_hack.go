be32468490bfe02257f4d516ace6d286bf41087e
Joel Hendrix <jhendrix@microsoft.com>
2020-06-23 11:03:29 -0700

    ambiguous imports fix

---
+++ modthing/go_mod_tidy_hack.go
@@ -0,0 +1,7 @@
+// +build modhack
+
+package modthing
+
+// Necessary for safely adding multi-module repo.
+// See: https://github.com/golang/go/wiki/Modules#is-it-possible-to-add-a-module-to-a-multi-module-repository
+import _ "github.com/jhendrixMSFT/go-modthing"
--- modthing/helper/go.mod
+++ modthing/helper/go.mod
@@ -1,3 +1,5 @@
 module github.com/jhendrixMSFT/go-modthing/modthing/helper
 
 go 1.14
+
+require github.com/jhendrixMSFT/go-modthing v3.1.0+incompatible
